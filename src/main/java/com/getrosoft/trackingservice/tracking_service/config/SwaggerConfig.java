427efdca1b5fe14bc27ebad4143a39415368a3a0
Bharath Ashok <iambharath.ashoka@gmail.com>
2024-12-04 21:27:29 +0530

    formatted properties file

--- src/main/java/com/getrosoft/trackingservice/tracking_service/config/SwaggerConfig.java
+++ src/main/java/com/getrosoft/trackingservice/tracking_service/config/SwaggerConfig.java
@@ -9,13 +9,15 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class SwaggerConfig {
 
+    private static final String SERVICE_DESCRIPTION = "The Tracking Service is a backend application designed to generate and manage tracking numbers for shipments. It provides REST APIs to handle tracking number generation, retrieval of tracking details, and validation of request parameters. The service is designed to be scalable, efficient, and extensible for logistics and e-commerce platforms.";
+
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
                 .info(new Info()
                         .title("Tracking Service API")
                         .version("1.0")
-                        .description("API documentation for Tracking Service")
+                        .description(SERVICE_DESCRIPTION)
                         .license(new License().name("Apache 2.0").url("http://springdoc.org")));
     }
 }
